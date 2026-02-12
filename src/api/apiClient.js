fe93ff554bf1e0ce7ad958b4c630671b204f1d08
Puneet Kushwaha <148247654+puneetkushwaha@users.noreply.github.com>
2026-02-12 18:37:33 +0530

    Update apiClient.js

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://fresh-mart-backend.onrender.com',
 });
 
 // Add a request interceptor to add the auth token to every request
